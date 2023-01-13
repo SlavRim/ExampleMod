ed3d3478571032f926c85a42ff51a04c300632ce
Sl4vP0weR <42337892+Sl4vP0weR@users.noreply.github.com>
2023-01-13 11:59:57 +0800

    Add PolySharp package. Update Hello world.

--- Mod.cs
+++ Mod.cs
@@ -1,4 +1,5 @@
 global using Mod = ExampleMod.Mod;
+using System.Runtime.CompilerServices;
 
 namespace ExampleMod;
 
@@ -8,6 +9,13 @@ public sealed class Mod : Verse.Mod
     public Mod(ModContentPack content) : base(content)
     {
         Instance = this;
-        Log.Message("Hello world!");
+        SayHelloWorld();
     }
+    static void SayHelloWorld([CallerFilePath] string file = null, [CallerMemberName] string member = null, [CallerLineNumber] int line = 0) => 
+        Log.Message(
+            ColoredText.Colorize(
+                $"Hello, world!\n{file} {member}:{line}",
+                Color.cyan
+            )
+        );
 }
